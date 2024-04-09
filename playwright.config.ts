5b19dc35585d5c861aaa8cff304609e1c66d664e
rogersrwr <160086701+rogersrwr@users.noreply.github.com>
2024-04-08 18:51:18 -0700

    yml ss try 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  snapshotPathTemplate: '{testDir}/lists_00.spec.ts-snapshots',
+  snapshotPathTemplate: '{testDir}/lists_00.spec.ts-snapshots/{arg}{ext}',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
