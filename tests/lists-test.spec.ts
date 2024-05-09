1093f629d840f3203da587031d1ad32bce40704c
rogersrwr <160086701+rogersrwr@users.noreply.github.com>
2024-05-09 13:40:03 -0700

    add emojis

--- tests/lists-test.spec.ts
+++ tests/lists-test.spec.ts
@@ -83,13 +83,13 @@ test.afterAll(async ({  }) => {
     await app.client.chat.postMessage({
       token: process.env.O_AUTH,
       channel: channelId,
-      text: `Tests ran successfully. Visit https://rogersrwr.github.io/BA-regressions-T6/ for full results.`,
+      text: `:white_check_mark: Tests ran successfully. Visit https://rogersrwr.github.io/BA-regressions-T6/ for full results.`,
     });
   } else if (jsonData.finished == true && jsonData.failures == true ) {
     await app.client.chat.postMessage({
       token: process.env.O_AUTH,
       channel: channelId,
-      text: `Test run has failed. Visit https://rogersrwr.github.io/BA-regressions-T6/ for full results.`,
+      text: `:x: Test run has failed. Visit https://rogersrwr.github.io/BA-regressions-T6/ for full results.`,
     });
   }
 });
