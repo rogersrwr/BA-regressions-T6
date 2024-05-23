cba3d47ab05c2ce5bedbfe38f9bb626d3043d8b8
rogersrwr <160086701+rogersrwr@users.noreply.github.com>
2024-05-23 15:42:33 -0700

    big rework

--- global-setup-preOpt.ts
+++ global-setup-preOpt.ts
@@ -11,7 +11,7 @@ const jsonData = require('D:/a/BA-regressions-T6/BA-regressions-T6/datetime.json
 
 
 test("setup checks", async ({ page }) => {
-  //test.slow();
+  test.slow();
   jsonData.datetime = "";
   jsonData.started = false;
   jsonData.failures = false;
@@ -150,86 +150,6 @@ test("setup checks", async ({ page }) => {
   await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
   await page.locator('#searchBarBtn').click();
   
-
-  // 011 Hide Disabled Contacts Button - CHECK AND MAKE SURE CHECKBOX IS SET TO DEFAULT 'UNCHECKED'
-  //ran in reverse order with 010 bc this impacts 010 check run
-  await page.getByRole('button', { name: 'ryan test' }).click();
-  const [request7] = await Promise.all([
-    page.waitForResponse(response => response.url().includes("TargetAPI/api/dialListContact/LoadContacts?accessToken=") && response.status() === 200, { timeout: 60000}),
-    page.getByRole('link', { name: 'test list 5'}).click()
-  ]);
-  //await expect(page.locator('p').filter({ hasText: 'Hide Disabled Contacts' }).getByTestId('CheckBoxOutlineBlankIcon')).toBeVisible();
-  if (await page.locator('p').filter({ hasText: 'Hide Disabled Contacts' }).getByTestId('CheckBoxIcon').isVisible()) {
-    await page.locator('#hideDisabledButton').uncheck();
-  }
-
-  await expect(page.getByRole('cell', { name: 'parent, phone' })).toBeVisible();
-  await expect(page.getByRole('cell', { name: 'phone, ryan' })).toBeVisible();
-
-  // 010 disable contact - make sure all contacts are enabled before test run. phone, ryan is contact that gets turned off
-  await page.getByTestId('MenuIcon').click();
-  await page.getByRole('button', { name: 'BrightArrow Central' }).click();
-  await page.locator('div').filter({ hasText: /^My Lists$/ }).click();
-  await page.getByRole('button', { name: 'ryan test' }).click();
-  const [request8] = await Promise.all([
-    page.waitForResponse(response => response.url().includes("TargetAPI/api/dialListContact/LoadContacts?accessToken=") && response.status() === 200, { timeout: 60000}),
-    page.getByRole('link', { name: 'test list 4'}).click()
-  ]);
-
-  if (await page.locator('tr').filter({ hasText: 'parent, phone8624385648' }).getByTestId('CheckBoxIcon').isHidden()) {
-    await page.locator('#cb_list492061456').uncheck();
-  }
-
-  if (await page.locator('tr').filter({ hasText: 'phone, ryan5555555557' }).getByTestId('CheckBoxIcon').isHidden()) {
-    //await page.locator('#cb_list438620451').check();
-    const [request8] = await Promise.all([
-      page.waitForResponse(response => response.url().includes("TargetAPI/api/dialListContact/EnableContact?accessToken=") && response.status() === 200, { timeout: 60000}),
-      page.locator('#cb_list492061458').check()
-    ]);
-  }
-
-
-  //008 reset favorites folder
-  await page.getByTestId('MenuIcon').click();
-  await page.getByRole('button', { name: 'BrightArrow Central' }).click();
-  await expect(page.getByText('Welcome, Ryan test')).toBeVisible();
-  //error to watch out for on line below
-  // Error: locator.click: Error: strict mode violation: locator('div').filter({ hasText: /^My Lists$/ }) resolved to 2 elements:
-  //   1) <div tabindex="0" role="button" class="MuiButtonBase-…>…</div> aka getByRole('button', { name: 'My Lists' })
-  //   2) <div class="MuiListItemText-root css-1tsvksn">…</div> aka getByRole('button', { name: 'My Lists' })
-  await page.locator('div').filter({ hasText: /^My Lists$/ }).click();
-  //await page.getByRole('button', { name: 'ryan test' }).click();
-
-  //await page.getByRole('button', { name: 'My Favorites' }).click();
-  const [request9] = await Promise.all([
-    page.waitForResponse(response => response.url().includes("TargetAPI/api/Folder/SetSelectedFolderSettings?accessToken=") && response.status() === 200, { timeout: 60000}),
-    page.getByRole('button', { name: 'My Favorites' }).click()
-  ]);
-  const myElement5 = page.locator('.listOfListsRow > td').first();
-  if (await myElement5.isVisible()) {
-    await page.getByRole('link', { name: 'test list' }).hover();
-    await page.getByRole('link', { name: 'Remove from folder' }).click();
-    const [request10] = await Promise.all([
-      page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/GetListsFromFolder?accessToken=") && response.status() === 200, { timeout: 60000}),
-      page.getByRole('button', { name: 'OK' }).click()
-    ]);
-  }
-
-  await page.getByRole('button', { name: 'ryan test' }).click();
-
-  //i forgot what below was 
-  // if (await page.locator('tr').filter({ hasText: 'Contact1, Auto' }).isVisible()) {
-  //   await page.getByRole('row', { name: 'Contact1, Auto 5555555555,' }).getByRole('button').nth(1).click();
-  //   await page.getByRole('button', { name: 'Yes' }).click();
-  //   await expect(page.getByRole('cell', { name: 'Contact1, Auto' })).not.toBeVisible();
-  // }
-
-  // if (await page.locator('tr').filter({ hasText: 'Contact2, Auto' }).isVisible()) {
-  //   await page.getByRole('row', { name: 'Contact2, Auto 5555555555,' }).getByRole('button').nth(1).click();
-  //   await page.getByRole('button', { name: 'Yes' }).click();
-  //   await expect(page.getByRole('cell', { name: 'Contact2, Auto' })).not.toBeVisible();
-  // }
-
 
 
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -26,8 +26,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  snapshotPathTemplate: '{testDir}/lists-test.spec.ts-snapshots/{arg}{ext}',
+  //reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
+  snapshotPathTemplate: '{testDir}/{testFileName}-snapshots/{arg}{ext}',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -52,6 +53,11 @@ export default defineConfig({
       name: 'chromium',
       dependencies: ['setup'],
       use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1080 },  },
+    },
+    {
+      name: 'chromium2',
+      use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1080 }, },
+      
     },
     // {
     //   name: "msg-check",
