b13e5b470ae554b75a4c2b89e86ae6473d631bca
rogersrwr <160086701+rogersrwr@users.noreply.github.com>
2024-06-02 11:25:27 -0700

    v 1.0 globalsetup

--- global-setup-V0_9.ts
+++ global-setup-V0_9.ts
@@ -34,13 +34,6 @@ test("setup checks", async ({ page }) => {
   await page.frameLocator('iframe[title="Help Scout Beacon - Messages and Notifications"]').getByRole('button', { name: 'Close' }).click();
 
 
-  
-  // sets starting folder to ryan test
-  await page.locator('div').filter({ hasText: /^My Lists$/ }).click();
-  await page.getByRole('button', { name: 'ryan test' }).click();
-  await page.getByTestId('MenuIcon').locator('path').click();
-  await page.getByRole('button', { name: 'BrightArrow Central' }).click();
-
   // 001 CREATE LIST (auto list 1) - CHECK IF NOT ALREADY DELETED
   await page.locator('div').filter({ hasText: /^My Lists$/ }).click();
   await page.getByRole('button', { name: 'ryan test' }).click();
@@ -75,7 +68,7 @@ test("setup checks", async ({ page }) => {
   
   //002 CREATE LIST (auto list 2) - CHECK IF NOT ALREADY DELETED
   await page.locator('#searchBarBtn').click();
-  await expect(page.getByRole('link', { name: 'test list 1', exact: true })).toBeVisible();  //new
+  await expect(page.getByRole('link', { name: 'test list 1' })).toBeVisible();  //new
   await page.getByRole('button', { name: 'ryan test' }).click();
   await page.getByLabel('Search').click();
   await page.getByLabel('Search').fill('auto list 2');
@@ -105,7 +98,7 @@ test("setup checks", async ({ page }) => {
     page.locator('#searchBarBtn').click()
   ]);
   await expect(page.getByRole('button', { name: 'ryan test' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'test list 1', exact: true })).toBeVisible();      //new
+  await expect(page.getByRole('link', { name: 'test list 1' })).toBeVisible();      //new
   await page.getByTestId('MenuIcon').click();
   await page.getByRole('button', { name: 'BrightArrow Central' }).click();
   await page.locator('div').filter({ hasText: /^My Lists$/ }).click();
@@ -130,7 +123,7 @@ test("setup checks", async ({ page }) => {
     await expect(page.getByText('Do you wish to delete the')).toBeVisible();
     await page.getByRole('button', { name: 'OK' }).click();
     //await expect(page.getByText('ryan test Lists (3)')).toBeVisible();
-    await expect(page.getByRole('link', { name: 'test list 1', exact: true })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'test list 1' })).toBeVisible();
     await page.getByRole('button', { name: 'ryan test' }).click();
   }
 
@@ -204,32 +197,17 @@ test("setup checks", async ({ page }) => {
   // Error: locator.click: Error: strict mode violation: locator('div').filter({ hasText: /^My Lists$/ }) resolved to 2 elements:
   //   1) <div tabindex="0" role="button" class="MuiButtonBase-…>…</div> aka getByRole('button', { name: 'My Lists' })
   //   2) <div class="MuiListItemText-root css-1tsvksn">…</div> aka getByRole('button', { name: 'My Lists' })
-
-  //await page.locator('div').filter({ hasText: /^My Lists$/ }).click();
-  const [request9_0] = await Promise.all([
-    page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/GetListsFromFolder?accessToken=") && response.status() === 200, { timeout: 60000}),
-    page.locator('div').filter({ hasText: /^My Lists$/ }).click()
-  ]);
-
+  await page.locator('div').filter({ hasText: /^My Lists$/ }).click();
   //await page.getByRole('button', { name: 'ryan test' }).click();
-  await expect(page.getByRole('link', { name: 'test list 1', exact: true })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'test list 2' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'test list 3' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'test list 4' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'test list 5' })).toBeVisible();
+
   //await page.getByRole('button', { name: 'My Favorites' }).click();
   const [request9] = await Promise.all([
     page.waitForResponse(response => response.url().includes("TargetAPI/api/Folder/SetSelectedFolderSettings?accessToken=") && response.status() === 200, { timeout: 60000}),
     page.getByRole('button', { name: 'My Favorites' }).click()
   ]);
-  await page.waitForResponse(response => response.url().includes("TargetAPI/api/Message/Logger") && response.status() === 200, { timeout: 60000});
-  await expect(page.getByRole('link', { name: 'test list 4' })).not.toBeVisible();  //sometimes there is split second window where api responds but page doesnt fully update
-  await expect(page.getByText('My Favorites Lists')).toBeVisible();
   const myElement5 = page.locator('.listOfListsRow > td').first();
-  //const myElement5 = page.getByRole('link', { name: 'test list' });
-  //const myElement5 = page.locator('_react=[id="btn_list8281415"]')
   if (await myElement5.isVisible()) {
-    await page.getByRole('link', { name: 'test list 1' }).hover();
+    await page.getByRole('link', { name: 'test list' }).hover();
     await page.getByRole('link', { name: 'Remove from folder' }).click();
     const [request10] = await Promise.all([
       page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/GetListsFromFolder?accessToken=") && response.status() === 200, { timeout: 60000}),
@@ -251,127 +229,6 @@ test("setup checks", async ({ page }) => {
   //   await page.getByRole('button', { name: 'Yes' }).click();
   //   await expect(page.getByRole('cell', { name: 'Contact2, Auto' })).not.toBeVisible();
   // }
-
-
-  //  subset list test teardown
-  await page.getByLabel('Search').click();
-  await page.getByLabel('Search').fill('subset list 1');
-  const [request11] = await Promise.all([
-    page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/GetListsFromFolder?accessToken=") && response.status() === 200, {timeout: 60000}),
-    page.getByLabel('Search').press('Enter')
-  ]);
-  await expect(page.getByRole('button', { name: 'test folder' })).toBeHidden();      //new
-
-  const myElement6 = page.locator('.listOfListsRow > td').first();
-  if (await myElement6.isVisible()) {
-    await page.locator('input[name="cb_lists2039717"]').check();
-    await page.getByRole('button', { name: 'Select an Action' }).click();
-    await page.getByRole('button', { name: 'Delete a list' }).click();
-    const [request12] = await Promise.all([
-      page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/DeleteList?accessToken=") && response.status() === 200, {timeout: 60000}),
-      page.getByRole('button', { name: 'OK' }).click()
-    ]);
-    await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
-    await page.locator('#searchBarBtn').click();
-    await page.locator('input[name="cb_lists2039717"]').uncheck();
-    await page.getByRole('button', { name: 'ryan test' }).click();
-    await page.getByLabel('Search').click();
-    await page.getByLabel('Search').fill('subset list 1');
-    await page.getByLabel('Search').press('Enter');
-  }
-  await expect(page.getByRole('button', { name: 'test folder' })).toBeHidden();         //new
-  await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
-
-
-  //  superset list test teardown
-  await page.getByLabel('Search').click();
-  await page.getByLabel('Search').fill('superset list 1');
-  const [request13] = await Promise.all([
-    page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/GetListsFromFolder?accessToken=") && response.status() === 200, {timeout: 60000}),
-    page.getByLabel('Search').press('Enter')
-  ]);
-  await expect(page.getByRole('button', { name: 'test folder' })).toBeHidden();      //new
-
-  const myElement7 = page.locator('.listOfListsRow > td').first();
-  if (await myElement7.isVisible()) {
-    await page.locator('input[name="cb_lists2039717"]').check();
-    await page.getByRole('button', { name: 'Select an Action' }).click();
-    await page.getByRole('button', { name: 'Delete a list' }).click();
-    const [request14] = await Promise.all([
-      page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/DeleteList?accessToken=") && response.status() === 200, {timeout: 60000}),
-      page.getByRole('button', { name: 'OK' }).click()
-    ]);
-    await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
-    await page.locator('#searchBarBtn').click();
-    await page.locator('input[name="cb_lists2039717"]').uncheck();
-    await page.getByRole('button', { name: 'ryan test' }).click();
-    await page.getByLabel('Search').click();
-    await page.getByLabel('Search').fill('superset list 1');
-    await page.getByLabel('Search').press('Enter');
-  }
-  await expect(page.getByRole('button', { name: 'test folder' })).toBeHidden();         //new
-  await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
-
-
-  //  test list 1: Filter 1 list test teardown
-  await page.getByLabel('Search').click();
-  await page.getByLabel('Search').fill('test list 1: Filter 1');
-  const [request15] = await Promise.all([
-    page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/GetListsFromFolder?accessToken=") && response.status() === 200, {timeout: 60000}),
-    page.getByLabel('Search').press('Enter')
-  ]);
-  await expect(page.getByRole('button', { name: 'test folder' })).toBeHidden();      //new
-
-  const myElement8 = page.locator('.listOfListsRow > td').first();
-  if (await myElement8.isVisible()) {
-    await page.locator('input[name="cb_lists2039717"]').check();
-    await page.getByRole('button', { name: 'Select an Action' }).click();
-    await page.getByRole('button', { name: 'Delete a list' }).click();
-    const [request16] = await Promise.all([
-      page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/DeleteList?accessToken=") && response.status() === 200, {timeout: 60000}),
-      page.getByRole('button', { name: 'OK' }).click()
-    ]);
-    await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
-    await page.locator('#searchBarBtn').click();
-    await page.locator('input[name="cb_lists2039717"]').uncheck();
-    await page.getByRole('button', { name: 'ryan test' }).click();
-    await page.getByLabel('Search').click();
-    await page.getByLabel('Search').fill('test list 1: Filter 1');
-    await page.getByLabel('Search').press('Enter');
-  }
-  await expect(page.getByRole('button', { name: 'test folder' })).toBeHidden();         //new
-  await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
-
-
-  //  #009-2 copy a list, test list 1 copy teardown
-  await page.getByLabel('Search').click();
-  await page.getByLabel('Search').fill('test list 1 copy');
-  const [request17] = await Promise.all([
-    page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/GetListsFromFolder?accessToken=") && response.status() === 200, {timeout: 60000}),
-    page.getByLabel('Search').press('Enter')
-  ]);
-  await expect(page.getByRole('button', { name: 'test folder' })).toBeHidden();      //new
-
-  const myElement9 = page.locator('.listOfListsRow > td').first();
-  if (await myElement9.isVisible()) {
-    await page.locator('input[name="cb_lists2039717"]').check();
-    await page.getByRole('button', { name: 'Select an Action' }).click();
-    await page.getByRole('button', { name: 'Delete a list' }).click();
-    const [request18] = await Promise.all([
-      page.waitForResponse(response => response.url().includes("TargetAPI/api/dialList/DeleteList?accessToken=") && response.status() === 200, {timeout: 60000}),
-      page.getByRole('button', { name: 'OK' }).click()
-    ]);
-    await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
-    await page.locator('#searchBarBtn').click();
-    await page.locator('input[name="cb_lists2039717"]').uncheck();
-    await page.getByRole('button', { name: 'ryan test' }).click();
-    await page.getByLabel('Search').click();
-    await page.getByLabel('Search').fill('test list 1 copy');
-    await page.getByLabel('Search').press('Enter');
-  }
-  await expect(page.getByRole('button', { name: 'test folder' })).toBeHidden();         //new
-  await expect(page.getByText('ryan test Lists (0)')).toBeVisible();
-
 
 
 
